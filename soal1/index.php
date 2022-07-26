9409f16655241d8d93ea1b1a8e7f9208ef67c741
hadlavine09 <haddadhikmah00@gmail.com>
2022-07-26 15:27:11 +0700

    soal_gist

--- soal1/index.php
+++ soal1/index.php
@@ -1,12 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+Biodata Pribadi : 
+
+Nama 			: Haddad Hikmah M
+Tempat & Tanggal Lahir 	: Bandung 23 AGUSTUS 2004
+Sekolah 		: SMK ASSALAAM BANDUNG
+Alamat Sekolah		: Jl.Situtarate - Terusan Cibaduyut , kab.Bandung - Jawa barat
+Tahun Masuk 		: 2019/2020
+Jurusan			: Rekayasa Perangkat Lunak (RPL)
+No Handphone		: 08986898483
+E-mail 			: haddadhikmah00@gmail.com
+No.KTP 			: -
+Alamat Tempat Tinggal 	: kec.baleendah desa.rancamanyar 
